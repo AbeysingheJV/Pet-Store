c8cfb5552fb7f72b0d87d117b2c4799278733999
AbeysingheJV <jvabeysinghe@students.nsbm.ac.lk>
2024-08-31 09:43:19 +0530

    Connected database

---
+++ backend/backend/Controllers/ProductsController.cs
@@ -0,0 +1,18 @@
+﻿using backend.Context;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace backend.Controllers
+{
+	[Route("api/[controller]")]
+	[ApiController]
+	public class ProductsController : ControllerBase
+	{
+		private readonly ApplicationDbContext _context;
+
+		public ProductsController(ApplicationDbContext context)
+		{
+			_context = context;
+		}
+	}
+}
---
+++ backend/backend/Entities/Product.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace backend.Entities
+{
+	public class Product
+	{
+		[Key]
+		public long Id { get; set; }
+
+		public string Brand { get; set; }
+
+		public string Title { get; set; }
+
+		public DateTime CreatedAt { get; set; }
+
+		public DateTime UpdatedAt { get; set; }
+	}
+}
