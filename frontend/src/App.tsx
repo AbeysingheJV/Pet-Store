814ea76087d173a8ca2e8c11b9d21c0383ef29ab
AbeysingheJV <jvabeysinghe@students.nsbm.ac.lk>
2024-09-03 11:30:34 +0530

    Created the navigation bar

---
+++ frontend/src/App.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import Navbar from './Components/navbar/navbar'
+
+const App: React.FC = () => {
+  return (
+    <div>
+      {/* Navbar */}
+      <Navbar></Navbar>
+
+      {/* Wrapper */}
+      <div className="wrapper">
+        {/* Routes */}
+      </div>
+    </div>
+  )
+}
+
+export default App
